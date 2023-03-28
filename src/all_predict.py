ceedf12d88be0adffd16028b5f6c910c95bbc8ab
TheBrightMoon <aikondrateva@mail.ru>
2023-03-28 20:24:26 +0300

    bert-realize

---
+++ src/all_predict.py
@@ -0,0 +1,119 @@
+import pandas as pd 
+import numpy as np
+import torch 
+
+from transformers import BertForSequenceClassification, BertTokenizer
+from keras_preprocessing.sequence import pad_sequences
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from src.preprocessing_data import checkpoint_file
+
+
+def all_predictions(df: pd.DataFrame, new_data_name: str, feature_column: list):
+
+    def get_mask(sent): 
+        """For each sentence create the attention mask"""
+        return [int(token_id > 0) for token_id in sent]
+
+    def convert_predictions(pred):
+        """Convert numpy arrays-tensors to real predictions"""
+
+        pred = pred[0]
+        pr_max = pred.max()
+        return [num for num, pr in enumerate(pred) if pr == pr_max][0] + 1 
+    
+    
+    if len(feature_column) => 2: 
+        pred_df = df.loc[:, feature_column]
+    elif len(feature_column) == 1: 
+        pred_df = df.loc[:, feature_column[0]]
+    else: 
+        print('Некорректный ввод feature_column')
+
+    tokenizer_name = 'DeepPavlov/rubert-base-cased-sentence'
+    model_path = 'model'
+
+    tokenizer = BertTokenizer.from_pretrained(tokenizer_name, do_lower_case=True)
+    model = BertForSequenceClassification.from_pretrained(
+        model_path, 
+        num_labels=8,   
+        local_files_only=True
+        )
+    
+    # Create sentence and label lists
+    sentences = df.deleted_sw.values
+
+    # if 'Подуровень' in feature_column: 
+    #     labels = df['Подуровень'].values
+    # else: labels = [] 
+
+    input_ids = list(map(lambda sent: tokenizer.encode(
+                            sent,                      
+                            add_special_tokens=True, 
+                    ), sentences))
+
+    # Pad our input tokens
+    MAX_LEN = 512 
+    input_ids = pad_sequences(input_ids, maxlen=MAX_LEN, 
+                            dtype="long", truncating="post", padding="post")
+
+
+    attention_masks = list(map(lambda x: get_mask(x), input_ids))
+
+    # Convert to tensors.
+    prediction_inputs = torch.tensor(input_ids)
+    prediction_masks = torch.tensor(attention_masks)
+    # prediction_labels = torch.tensor(labels)
+
+    # Set the batch size.  
+    batch_size = 1
+
+    # Create the DataLoader.
+    # prediction_data = TensorDataset(prediction_inputs, prediction_masks, prediction_labels)
+    prediction_data = TensorDataset(prediction_inputs, prediction_masks)
+    prediction_sampler = SequentialSampler(prediction_data)
+    prediction_dataloader = DataLoader(prediction_data, sampler=prediction_sampler, batch_size=batch_size)
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    if device == 'cuda': 
+        model.cuda()
+    else: model.cpu()
+
+    model.eval()
+
+    # Tracking variables 
+    predictions = []
+
+    # Predict 
+    for batch in prediction_dataloader:
+    # Add batch to GPU
+        batch = tuple(t.to(device) for t in batch)
+        
+        # Unpack the inputs from our dataloader
+        b_input_ids, b_input_mask = batch
+        
+        # Telling the model not to compute or store gradients, saving memory and 
+        # speeding up prediction
+        with torch.no_grad():
+            # Forward pass, calculate logit predictions
+            outputs = model(
+                b_input_ids, token_type_ids=None, 
+                attention_mask=b_input_mask
+                )
+
+        logits = outputs[0]
+
+        # Move logits and labels to CPU
+        logits = logits.detach().cpu().numpy()
+        # label_ids = b_labels.to('cpu').numpy()
+        
+        # Store predictions
+        predictions.append(logits)
+        # true_labels.append(label_ids)
+
+    model_preds = np.array(list(map(lambda x: convert_predictions(x), predictions)))
+    # true_labels = np.array(true_labels)
+    pred_df['Predictions'] = model_preds
+
+    saved = checkpoint_file(df=pred_df, file_name=new_data_name)
+
+    return pred_df
---
+++ src/preprocessing_data.py
@@ -0,0 +1,125 @@
+import os 
+import re
+import string
+import pandas as pd 
+import pandas as pd
+import numpy as np
+import string
+import nltk
+
+from nltk.corpus import stopwords
+nltk.download("stopwords")
+nltk.download('punkt')
+
+from sklearn.model_selection import train_test_split
+from tqdm.auto import tqdm
+from nltk.stem import *
+from nltk.corpus import stopwords
+
+# from transformers import AutoTokenizer
+from pymystem3 import Mystem
+
+
+def remove_stop_words(text, stopwords_list):
+    wds = [w for w in text.split() if w not in stopwords_list and w != ' ']
+
+    return " ".join(wds)
+
+def checkpoint_file(df, file_name): 
+    return df.to_csv(file_name)
+
+def loading_data(data_name: str) -> pd.DataFrame: 
+    """Check and load your data"""
+
+    if data_name.endswith('.xlsx'): 
+        df = pd.read_excel(data_name, engine='openpyxl')
+
+    elif data_name.endswith('.csv'): 
+        df = pd.read_csv(data_name)
+
+    return df 
+
+def remove_punctuation(text):
+    return ''.join(
+        [ch if ch not in string.punctuation else ' ' for ch in text]
+        )
+
+def remove_numbers(text):
+    return ''.join([i if not i.isdigit() else ' ' for i in text])
+
+def remove_multiple_spaces(text):
+	return re.sub(r'\s+', ' ', text, flags=re.I)
+
+def stop_words_deleting(df: pd.DataFrame, prep_col: str): 
+    """Deleting a whole stopwords with abbreviation checking"""
+
+    def delete_stop_words(text: str, stopwords): 
+        text =' '.join(
+                [word.lower() for word in text.split() 
+                if (word.lower() not in stopwords) & 
+                (word not in string.punctuation)]
+            )
+        return text
+    
+    russian_stopwords = stopwords.words("russian")
+    russian_stopwords.extend(
+        ['•', '…', '«', '»', '...', 'т.д.', 'т', 'д', 'так', 
+        'нон', 'сыр', 'сур', 'сол', 'мг', 'доз', 'бет', 
+        '-', '№', '—'
+        ]
+    )
+        
+    # Cyrillic and latin single characters: 
+    a = ord('а')
+    cyrillic = [chr(i) for i in range(a,a+32)]
+    latin = list(string.ascii_lowercase)
+    russian_stopwords.extend(cyrillic)
+    russian_stopwords.extend(latin)
+
+    without_sw = df[prep_col].map(delete_stop_words)
+    return without_sw
+
+def residual_preprocess(df: pd.DataFrame, col: str): 
+    def residual(text): 
+        return ' '.join([w for w in text.split() if len(w) not in (2, 3)])
+    
+    return np.array(df[col].map(residual), dtype=str)
+
+def all_preprocessing(data_name: str, feature_columns: list, 
+                      new_data_name: str):
+
+    df = loading_data(data_name)
+    # df = df.loc[:, feature_columns.extend('Подуровень')]
+
+    if len(feature_columns) == 2: 
+        feature1, feature2 = feature_columns[0], feature_columns[1]
+        df[feature1] = df[feature1] + df[feature2]
+        feature_column = feature1
+    elif len(feature_columns) == 1: 
+        feature_column = feature_columns[0]
+    elif isinstance(feature_columns, 'str'): 
+        feature_column = feature_columns
+
+    prep_text = [
+        remove_multiple_spaces(
+        remove_numbers(
+        remove_punctuation(text)))
+        for text in tqdm(df[feature_column])
+    ]
+    df[f'prep_text'] = prep_text
+
+    # stemmer = SnowballStemmer("russian") 
+
+    df[f'deleted_sw'] = stop_words_deleting(
+        df=df, 
+        prep_col=f'prep_text'
+    )
+        
+    df[f'deleted_sw'] = residual_preprocess(
+        df=df, col=f'deleted_sw'
+    )
+
+        
+    df['text_lemm'] = lemm_texts_list
+    
+    return df
